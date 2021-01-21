7d51fd864d2113d7765e8c995435108753156661
Arinonia <arinonia.yt@gmail.com>
2021-01-21 01:00:14 +0100

    :heavy_plus_sign: add readme licence ect...

--- src/main/java/fr/ordinal/terminal/utils/Constants.java
+++ src/main/java/fr/ordinal/terminal/utils/Constants.java
@@ -5,7 +5,7 @@ import com.jcraft.jsch.JSch;
 public class Constants {
 
     public static final String APP_NAME = "Ordinal-Terminal";
-    public static final String APP_VERSION = "0.0.1";
+    public static final String APP_VERSION = "0.0.1-SNAPSHOT";
     public static final double MIN_WIDTH = 1080;
     public static final double MIN_HEIGHT = 720;
 
